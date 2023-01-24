a004b3e7aaec33945993a3250dc4939779bd4b9f
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2023-01-24 05:35:10 +0000

    Update

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(window).scroll(function() {
+    var $window = $(window);
+
+    var scroll = $window.scrollTop() + ($window.height() / 3);
+
+    if(parseInt(scroll) > 800 && parseInt(scroll) < 2510){
+        document.querySelector('.vtNavBar').style.backgroundColor = "rgb(24, 23, 23)";
+    } else if(parseInt(scroll)> 2510) {
+        document.querySelector('.vtNavBar').style.backgroundColor = "black";
+    } else{
+        document.querySelector('.vtNavBar').style.backgroundColor = "black";
+    }
+
+});
+
+
+function eventDesignA() {
+    document.getElementById("dsgXS").style.height = "100px";
+} 
+
+function eventDesignAout() {
+    document.getElementById("dsgXS").style.height = "5px";
+}
+
+function eventDesignB() {
+    document.getElementById("dsgMS").style.height = "100px";
+}
+
+function eventDesignBout() {
+    document.getElementById("dsgMS").style.height = "5px";
+}
+
+function eventDesignC() {
+    document.getElementById("dsgCI").style.height = "100px";
+} 
+
+function eventDesignCout() {
+    document.getElementById("dsgCI").style.height = "5px";
+}
